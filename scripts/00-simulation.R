08c4cc574ec3428e097ba71bdaef04d9ea1c42fd
Xiao Bai <x.bai@mail.utoronro.ca>
2022-04-11 02:03:48 -0400

    Final Product!

--- scripts/00-simulation.R
+++ scripts/00-simulation.R
@@ -4,6 +4,62 @@ library(lubridate)
 library(tidyverse)
 library(tidyr)
 
+
+
+df1_new<-as.data.frame(t(data_region))
+df1_new
+
+
+data_region%>%
+  summarise(mean_no_edu = mean(no_education), mean_higher_edu = mean(completed_primary))
+#test_stat = 31.79-27.335 = 4.455
+
+mean_data <- data.frame (characteristic  = c("no_education", "completed_prim"),
+                         means = c("31.79", "27.335")
+)%>%mutate(means = as.numeric(means))
+
+test_stat = as.numeric(mean_data%>%summarise(test_stat = diff(means)))
+test_stat
+
+
+repetitions = 1000
+simulated_stats = rep(NA, repetitions)
+for (i in 1:repetitions)
+{
+  dat =mean_data%>% mutate(characteristic=sample(characteristic))
+  y = dat %>%
+    summarise(sim_test_stat = diff(means))
+  x[i]=as.numeric(y)
+}
+sim = data.frame(mean_diff=x)
+
+sim%>%
+  ggplot(aes(x=mean_diff))+geom_histogram()
+
+
+
+
+t <- (6.82381	-13.4)/(5.586124/sqrt(43))
+pvalue <- 1-pt(t,1000-1)
+pvalue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 set.seed(234)
 
 simulated_Tanzania_data <- 
@@ -22,13 +78,17 @@ simulated_Tanzania_data <-
     year = 
       rep(c(1990:2009), 8),
     
-    percent_male_no_education = 
+    no_education = 
       runif(n = 160,
             min = 10.2, 
             max = 50.04),
-    percent_male_primary_education = 
+    primary_incompleted = 
       runif(n = 160,
             min = 13.2, 
-            max = 50.3)
+            max = 50.3),
     
+    primary_completed = 
+      runif(n = 160,
+            min = 10.2, 
+            max = 50.3)
   )
